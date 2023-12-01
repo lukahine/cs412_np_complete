48c9c68e06d45aba6c14aa76bd14c94e9db22d93
MyPseudoNameIsNick <hamiltnc@dukes.jmu.edu>
2023-12-01 10:01:51 -0500

    Update cs412_approx1.py
    Added a random Vertex selector. Having issues with implementing nearest neighbor.

--- approx_1/cs412_approx1.py
+++ approx_1/cs412_approx1.py
@@ -5,16 +5,23 @@
 
         Comments
 """
-
+import random
 
 def approxing(G, currVert):
-    pass
+    visited = [currVert]
+    minVal = 0
+    while (len(visited) <= len(G)):
+        for edge in G[currVert]:
+            minVal = min(G[currVert].values())
+            print("The min value in this group is " + str(minVal))
+        print(visited)
+        visited.append(list(G[currVert].values()).index(minVal))
     
-
 
 def main():
     G = {}
     numVertices, numEdges = [int(x) for x in input().split()]
+    listForRandom = []
             
     for _ in range(numEdges):
         theInput = input().split()
@@ -23,12 +30,16 @@ def main():
         w = int(theInput[2])
         if (u not in G):
             G[u] = {}
+            listForRandom.append(u)
         if (v not in G):
             G[v] = {}
+            listForRandom.append(v)
         w = int(w)
         G[u][v] = w
         G[v][u] = w
+    randomVertex = listForRandom[random.randint(0, len(G) - 1)]
 
+    approxing(G, randomVertex)
     print(G)
 
 if __name__ == "__main__":
