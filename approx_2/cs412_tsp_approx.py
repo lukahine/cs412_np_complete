7b1cbcf4374a86f24fded4d20fcad6dc10f1d5ab
hinelp <hinelp@dukes.jmu.edu>
2023-12-07 15:26:55 -0500

    Update cs412_tsp_approx.py

--- approx_2/cs412_tsp_approx.py
+++ approx_2/cs412_tsp_approx.py
@@ -10,18 +10,22 @@
 
 import networkx as nx
 
-
 def minimum_spanning_tree(graph):
-    return nx.minimum_spanning_tree(graph)
-
+    return nx.minimum_spanning_tree(graph, algorithm="prim")
 
 def minimum_weight_perfect_matching(odd_vertices, graph):
-    return nx.max_weight_matching(graph, maxcardinality=False)
+    negated_graph = nx.Graph()
+    for u, v, w in graph.edges(data=True):
+        negated_graph.add_edge(u, v, weight=-w["weight"])
 
+    matching_edges = nx.max_weight_matching(negated_graph, maxcardinality=False)
+
+    matching_edges_with_original_weights = [(u, v, -w["weight"]) for u, v, w in matching_edges]
+
+    return matching_edges_with_original_weights
 
 def eulerian_circuit(graph):
     return list(nx.eulerian_circuit(graph))
-
 
 def shortcut_eulerian_circuit(eulerian_circuit):
     visited = set()
@@ -38,13 +42,12 @@ def shortcut_eulerian_circuit(eulerian_circuit):
 
     return hamiltonian_circuit
 
-
 def christofides_algorithm(graph):
     # Step 1: Minimum Spanning Tree
     mst = minimum_spanning_tree(graph)
 
     # Step 2: Minimum Weight Perfect Matching for Odd-Degree Vertices
-    # Algorithm was written expecting an incomplete connected graph, so the odd degree vertices doesn't really matter here
+    # Algorithm was written expecting an incomplete connected graph, so the odd degree vertices don't really matter here
     odd_degree_vertices = [v for v, d in mst.degree if d % 2 == 1]
     perfect_matching = minimum_weight_perfect_matching(odd_degree_vertices, graph)
 
@@ -52,7 +55,7 @@ def christofides_algorithm(graph):
     multigraph = nx.MultiGraph(mst)
     multigraph.add_edges_from(perfect_matching)
 
-    # Make sure the multigraph is Eulerian
+    # Make sure an Eulerian circuit can be found in the graph
     if not nx.is_eulerian(multigraph):
         odd_degree_nodes = [
             node for node, degree in multigraph.degree() if degree % 2 == 1
@@ -68,27 +71,23 @@ def christofides_algorithm(graph):
 
     return hamiltonian_circuit
 
-
 def main():
     G = nx.Graph()
     numVertices, numEdges = map(int, input().split())
 
     for _ in range(numEdges):
         u, v, w = input().split()
-        G.add_edge(u, v, weight=int(w))
+        G.add_edge(u, v, weight=float(w))
 
-    # Step 5: Apply Christofides Algorithm
     hamiltonian_circuit = christofides_algorithm(G)
 
-    # Calculate total cost
-    total_cost = sum(
-        G[hamiltonian_circuit[i]][hamiltonian_circuit[i + 1]]["weight"]
-        for i in range(len(hamiltonian_circuit) - 1)
-    )
+    total_cost = 0.0
+    for i in range(len(hamiltonian_circuit) - 1):
+        edge_weight = G[hamiltonian_circuit[i]][hamiltonian_circuit[i + 1]]["weight"]
+        total_cost += edge_weight
 
     print(total_cost)
     print(" ".join(map(str, hamiltonian_circuit)))
-
 
 if __name__ == "__main__":
     main()
